be6ae263c82d41aa9626e5e264a4db38641ef20c
Balint Orosz <orosz.balint.97@gmail.com>
2023-10-17 19:13:18 +0200

    add pipeline

--- lib/cdk-platform-stack.ts
+++ lib/cdk-platform-stack.ts
@@ -5,13 +5,32 @@ import {
   LambdaDeploymentConfig,
   LambdaDeploymentGroup,
 } from "aws-cdk-lib/aws-codedeploy";
+import { Pipeline } from "aws-cdk-lib/aws-codepipeline";
 import { Alias } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
 export class CdkPlatformStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const pipeline = new Pipeline(this, "pipeline");
+
+    new CodePipeline(this, "code-pipeline", {
+      codePipeline: pipeline,
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.connection("blntrsz/cdk-platform", "main", {
+          connectionArn:
+            "arn:aws:codestar-connections:eu-central-1:155601209279:connection/51dc3226-ffdd-459d-a034-50f9ff503d2b",
+        }),
+        commands: ["npm ci", "npx cdk synth"],
+      }),
+    });
 
     const lambda = new NodejsFunction(this, "lambda", {
       currentVersionOptions: {
