63f94d771bb88dc54724a1326c7565ea385604d5
Balint Orosz <orosz.balint.97@gmail.com>
2023-10-17 20:39:30 +0200

    emulate error

--- lib/lambda-stack.lambda.ts
+++ lib/lambda-stack.lambda.ts
@@ -1,6 +1,7 @@
 import { Handler } from "aws-cdk-lib/aws-lambda";
 
 export const handler: Handler = () => {
+  throw new Error("Emulate Error");
   return {
     statusCode: 200,
     body: JSON.stringify({
