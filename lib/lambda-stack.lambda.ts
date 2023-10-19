98f17df6ab90cdd280d38d0d975c9538605b37f2
Balint Orosz <orosz.balint.97@gmail.com>
2023-10-19 20:46:12 +0200

    simulate error

--- lib/lambda-stack.lambda.ts
+++ lib/lambda-stack.lambda.ts
@@ -1,6 +1,7 @@
 import { Handler } from "aws-cdk-lib/aws-lambda";
 
 export const handler: Handler = async () => {
+  throw new Error("Simulate Error");
   return {
     statusCode: 200,
     body: "OK",
